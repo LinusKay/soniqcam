86287cdb6c6be19939477535048b625191a64929
Linus Kay <s3668218@student.rmit.edu.au>
2023-10-26 15:43:23 +1100

    Add initial files

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include('header.php'); ?>
+<?php 
+// SET NUMBER OF AVAILABLE CAMERAS
+// If stream image not provided in stream/camera<number>.jpg, "Offline" placeholder will be set
+$cams = 3;
+
+// Currently selected camera
+$stream = 1;
+if(isset($_GET['stream'])) {
+    if($_GET['stream'] > $cams) {
+        $stream = 1;
+    }
+    else {
+        $stream = $_GET['stream'];
+    } 
+}
+$stream_image = "stream/camera_" . $stream . "_latest.jpg";
+if(!file_exists($stream_image)) {
+    $stream_image = "media/offline.jpg";
+}
+?>
+        <section id="controls">
+            <form>
+                <select name="stream" onchange="this.form.submit()">
+                    <?php 
+                        for($cam=1; $cam <= $cams; $cam++) {
+                            if($stream == $cam) {
+                                echo "<option selected value=" . $cam . ">Source " . $cam . "</option>";
+                            }
+                            else {
+                                echo "<option value=" . $cam . ">Source " . $cam . "</option>";
+                            }
+                        }
+                    ?>
+                </select>
+            </form>
+        </section>
+        <article id="cam-box">
+            <img class="cam-frame" src=<?php echo $stream_image;?>>
+        </article>
+<?php include('footer.php'); ?>
